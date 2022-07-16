786c5a6e55c28d93c20fa03635773b0eecaf110a
larosa <2903482792@qq.com>
2022-07-16 13:32:29 +0800

    循环功能

--- Main.java
+++ Main.java
@@ -2,8 +2,10 @@ import java.util.*;
 public class Main{
 	public static void main(String [] args)
 	{
-		System.out.println("Hello world");
-		
+		for(int i=0;i<5;i++) {
+		System.out.println("Hello world");	
+			
+		}
 	}	
 	
 }
